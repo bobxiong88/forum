767041cb5b2bd851d82ce93c4f926bfe90f8ba31
bobxiong88 <39507662+bobxiong88@users.noreply.github.com>
2022-10-30 18:20:36 -0400

    Update boards.js

--- src/components/boards.js
+++ src/components/boards.js
@@ -1,21 +1,9 @@
 const boards = {
     "pol": {
-        name: "Politically Incorrect",
+        name: "World News",
         description:  
         `
-        This board is for the discussion of news, world events, political issues, and other related topics.
-
-        Off-topic and /b/-tier threads will be deleted (and possibly earn you a ban, if you persist). Unless they are quality, well thought out, well written posts, the following are some examples of off-topic and/or /b/-tier threads:
-
-        >Red pill me on X. (with no extra content or input of your own)
-        >Are X white?
-        >Is X degeneracy?
-        >How come X girls love Y guys so much?
-        >If X is true, then how come Y? Checkmate Z.
-
-        The variety of threads allowed here are very flexible and we believe in freedom of speech, but we expect a high level of discourse befitting of the board. Attempts to disrupt the board will not be tolerated, nor will calls to disrupt other boards and sites.
-
-        If you want a place to discuss topics not related to news, world events, or politics, please try /bant/ - International/Random,
+        This board is for the discussion of news and world events.
         `,
         desImg: "pol.jpg" 
     },
